a4d0650c214b9c789b300e470e05d665fe646e4b
katsikora <katsikora@users.noreply.github.com>
2017-03-29 18:22:51 +0200

    Add files via upload
    added_functions

--- SC_RNAseq_various.R
+++ SC_RNAseq_various.R
@@ -26,3 +26,43 @@ plotexptsne_facet<-function(scobject,genestring,genetitle,facetvar,logsc=FALSE){
     }
 
 ########################################################################################################
+
+
+######################ANNOTATE MONOCLE DIFFERENTIALLY EXPRESSED GENE TABLE WITH CELL COUNTS #############
+#NOT TESTED
+#will annotate a differentially expressed gene table from monocle with cell counts and log2FC in marker-positive cells for a grouping variable in pData of monocle object, for which the DEG table was generated
+#assumes two levels of grouping variable (i.e. will take the first two levels)
+#cells with mincount transcripts per gene will be used to calculate group means and log2FC (mean group 1: mean group 2)
+#pData of the monocle object should have a column "SampleID", the entries of which match column names of expression data
+#returns annotated data frame
+
+annotate_DEG_list<-function(monocleObject,monocleDEGtable,groupingVar,mincount){
+
+    require(monocle)
+    exp.dat.filt<-as.matrix(monocleObject)
+    tabz<-data.frame(matrix(nrow=nrow(monocleDEGtable),ncol=9))
+    gVu<-unique(as.character(pData(monocleObject)[,groupingVar]))
+    colnames(tabz)<-c(paste0("Expr.",gVu[1]),paste0("Expr.",gVu[2]),paste0("NCells2.",gVu[1]),paste0("NCells2.",gVu[2]),"log2FC","GeneSym",paste0("Expr.",gVu[1],".iC"),paste0("Expr.",gVu[2],".iC","log2FC.iC")
+    exp.A<-exp.dat.filt[,colnames(exp.dat.filt) %in% as.character(pData(monocleObject)$SampleID)[as.character(pData(monocleObject)[,groupingVar]) %in% gVu[1]]]
+    exp.B<-exp.dat.filt[,colnames(exp.dat.filt) %in% as.character(pData(monocleObject)$SampleID)[as.character(pData(monocleObject)[,groupingVar]) %in% gVu[2]]]
+    for( k in seq_along(1:nrow(monocleDEGtable))){
+        tabz[k,paste0("Expr.",gVu[1])]<-mean(exp.A[rownames(exp.A)==rownames(monocleDEGtable)[k],])
+        tabz[k,paste0("Expr.",gVu[2])]<-mean(exp.B[rownames(exp.B)==rownames(monocleDEGtable)[k],])    
+        tabz[k,paste0("NCells2.",gVu[1])]<-sum(exp.A[rownames(exp.A)==rownames(monocleDEGtable)[k],]>=mincount)
+        tabz[k,paste0("NCells2.",gVu[2])]<-sum(exp.B[rownames(exp.B)==rownames(monocleDEGtable)[k],]>=mincount)    
+        tmp.A<-exp.A[rownames(exp.A)==rownames(monocleDEGtable)[k],]
+        tmp.B<-exp.B[rownames(exp.B)==rownames(monocleDEGtable)[k],]    
+        tabz[k,paste0("Expr.",gVu[1],".iC")]<-mean(tmp.A[tmp.A>=mincount])
+        tabz[k,paste0("Expr.",gVu[2],".iC")]<-mean(tmp.B[tmp.B>=mincount])
+        print(paste0(k,"_processed"))
+        }
+    tabz[is.na(tabz)]<-0.01
+    tabz$log2FC<-log2(tabz[k,paste0("Expr.",gVu[1])]/tabz[k,paste0("Expr.",gVu[2])])
+    tabz$log2FC.iC<-log2(tabz[k,paste0("Expr.",gVu[1],".iC")]/tabz[k,paste0("Expr.",gVu[2],".iC")])
+    tabz.an<-data.frame(v,tabz,stringsAsFactors=FALSE)
+    rownames(tabz.an)<-rownames(monocleDEGtable)
+    return(tabz.an)
+}
+
+################################################################################################################################
+
